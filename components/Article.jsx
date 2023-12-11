45880a15c89db0ab84eaf63fd3eca2e58e286dad
matt12171 <matthewpatterson@hotmail.co.uk>
2023-12-11 17:39:17 +0000

    Add article page

--- components/Article.jsx
+++ components/Article.jsx
@@ -2,8 +2,47 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { api } from "./Home";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
 export const Article = () => {
   const { article_id } = useParams();
-  return <div>{article_id}</div>;
+  const [singleArticle, setSingleArticle] = useState("");
+  const [loading, setLoading] = useState("");
+  const [topVote, setTopVote] = useState("");
+
+  useEffect(() => {
+    api.get(`/articles/${article_id}`).then((response) => {
+      console.log(response.data);
+      setSingleArticle(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get(`/articles/${article_id}/comments`).then((response) => {
+      console.log(response.data.comments);
+      let maxVotes = response.data.comments[0].votes;
+      let maxVotedObject = null;
+      response.data.comments.forEach((currentObject) => {
+        if (currentObject.votes > maxVotes) {
+          maxVotes = currentObject.votes;
+          maxVotedObject = currentObject;
+        }
+      });
+      console.log(maxVotedObject);
+      setTopVote(maxVotedObject);
+    });
+  }, []);
+  return (
+    <div className="card">
+      <h3>{singleArticle.title}</h3>
+      <p>Submitted by {singleArticle.author}</p>
+      <img className="article-img" src={`${singleArticle.article_img_url}`} />
+      <p>{singleArticle.body}</p>
+      <h4>Top Comment</h4>
+      <p>{topVote.body}</p>
+      <Link to="">See all comments..</Link>
+    </div>
+  );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from "../components/Header";
 import { PanelButtons } from "../components/PanelButtons";
 import "./App.css";
 
+
 function App() {
   return (
     <div>
